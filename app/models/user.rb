3d3e2bd0fc967665ebcf398caee1230749f263ef
HironoriTanaka <300ldrive@gmail.com>
2023-12-25 22:31:40 +0900

    ログイン・ログアウト機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :item_purchases
+  has_many :items
+
+  validates :nickname, presence: true
+  validates :email, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください', presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'に全角文字を使用してください' } do
+    validates :lastname_kanji, presence: true
+    validates :firstname_kanji, presence: true
+    validates :lastname_kana, presence: true
+    validates :firstname_kana, presence: true
+  end
+
+  validates :birth, presence: true
+end
