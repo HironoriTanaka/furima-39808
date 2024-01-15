a1d8999264788bbf4aa609448a810c985ab53f23
HironoriTanaka <300ldrive@gmail.com>
2024-01-15 21:23:21 +0900

    011501編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:edit, :update, :show]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -18,12 +19,31 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    if user_signed_in?
+      if current_user.id != @item.user_id
+        redirect_to root_path
+      end
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def show
-    @item = Item.find(params[:id])
   end
 
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :postage_type_id, :prefecture_id, :preparation_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
